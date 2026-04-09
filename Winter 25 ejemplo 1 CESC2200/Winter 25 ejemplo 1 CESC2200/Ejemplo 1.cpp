8b556c30bdc81df9e431fce1006f6aace9a8a02d
Null-27 <dennisosorio93@gmail.com>
2026-04-09 15:38:24 -0400

    first commit

---
+++ Winter 25 ejemplo 1 CESC2200/Winter 25 ejemplo 1 CESC2200/Ejemplo 1.cpp
@@ -0,0 +1,20 @@
+ #include <iostream>
+using namespace std;
+
+int main()
+{
+	double l, an, A;
+	
+	cout << " Entre el largo del rect: ";
+	cin >> l;
+	
+	cout << "Entre el ancho del rect: ";
+	cin >> an;
+
+	A = l * an;
+
+	cout << "El area del rect. es " << A;
+
+	return 0;
+
+}
